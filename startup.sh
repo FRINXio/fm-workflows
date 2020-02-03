48646d8d0566d3d406e90e5cdab4dd814d7da1de
Tomas <thodor@frinx.io>
2020-02-03 14:02:34 +0100

    Add wait after restarting micros container
    Signed-off-by: Tomas <thodor@frinx.io>

--- startup.sh
+++ startup.sh
@@ -7,6 +7,32 @@ function copy_workers {
   docker cp workers/. micros:/home/app/netinfra_utils/workers
 }
 
+curl_workers_copy=(curl --silent --write-out 'HTTPSTATUS:%{http_code}' -X GET 'http://127.0.0.1:8080/api/metadata/taskdefs/UNIFIED_check_unified_node_exists')
+
+function check_container {
+local cmd=$1[@]
+
+for i in {100..1}; do
+
+  response=$(${!cmd})
+  echo -ne "Waiting for $1 to respond. For $i seconds\033[0K\r"
+
+  HTTP_STATUS=$(echo $response | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+  if [ $HTTP_STATUS -eq 200 ];
+  then
+    echo
+    echo "Microservices is ready"
+    return 0
+  fi
+  sleep 1
+done
+
+echo
+echo "Microservices not healthy"
+return 1
+
+}
+
 function import_workflows {
   docker cp workflows/. micros:/home/app/netinfra_utils/workflows
   docker cp importWorkflow.sh micros:/home/app/netinfra_utils/
@@ -23,13 +49,15 @@ function import_devices {
 # Copy workers into running container
 copy_workers
 
+# Restart micros to register workers
+docker container restart micros
+
+check_container curl_workers_copy
+
 # Imports workflows
 import_workflows
 
 # Import devices
 import_devices
-
-# Restart micros to register workers
-docker container restart micros
 
 echo -e '\nDemo workers/workflows/devices successfully imported!\n'
