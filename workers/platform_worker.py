7d8f8fb1c5e2f3ff68727a8d2ea8f1dbeecbf307
Tomas <thodor@frinx.io>
2020-02-05 16:02:32 +0100

    Uniconfig transactions
    Signed-off-by: Tomas <thodor@frinx.io>

--- workers/platform_worker.py
+++ workers/platform_worker.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 import requests
-from frinx_rest import odl_url_base, odl_headers, odl_credentials, parse_response
+from frinx_rest import odl_url_base, odl_credentials, parse_response, add_uniconfig_tx_cookie
 
 from string import Template
 
@@ -10,10 +10,11 @@ odl_url_components = odl_url_base + "/data/network-topology:network-topology/top
 
 def read_components(task):
     device_id = task['inputData']['device_id']
+    uniconfig_tx_id = task['inputData']['uniconfig_tx_id'] if 'uniconfig_tx_id' in task["inputData"] else ""
 
     id_url = Template(odl_url_components).substitute({"id": device_id})
 
-    r = requests.get(id_url, headers=odl_headers, auth=odl_credentials)
+    r = requests.get(id_url, headers=add_uniconfig_tx_cookie(uniconfig_tx_id), auth=odl_credentials)
     response_code, response_json = parse_response(r)
 
     if response_code == requests.codes.ok:
@@ -41,7 +42,8 @@ def start(cc):
         "retryDelaySeconds": 0,
         "responseTimeoutSeconds": 10,
         "inputKeys": [
-            "device_id"
+            "device_id",
+            "uniconfig_tx_id"
         ],
         "outputKeys": [
             "url",
