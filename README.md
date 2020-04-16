46cb959d2f8a4b569ba9696586d2281b43256256
Stanislav Chlebec <schlebec@frinx.io>
2020-04-16 11:57:56 +0200

    add e2e tests for FRINX-machine
    Signed-off-by: Stanislav Chlebec <schlebec@frinx.io>

--- README.md
+++ README.md
@@ -2,3 +2,66 @@
 
 ## Usage
 Make sure FRINX-machine is running, navigate to `cd /fm-workflows` and execute `./startup.sh`. Imported workflows and tasks will appear in FRINX-Machine UI, right after importing finishes.
+
+Note:
+If you use FRINX-machine v1.1 please issue the command
+```
+git checkout 8a611581b3d8b7f75e3348e4723dbf756c3ea02e #valid for downloaded FRINX-machine v1.1
+```
+to avoid of error messages connected with later incompatible changes.
+
+## Cypress e2e tests
+In the cypress folder there are GUI tests.
+
+### Install Cypress
+See [Cypress][cypress]
+
+Install optionally prerequisites for cypress (only if you experience problems with during cypress run):
+```
+sudo apt-get -y install libgtk-3-0 libnotify-dev libgconf-2-4 libxss1 xvfb
+
+VERSION=v12.16.1
+DISTRO=linux-x64
+wget https://nodejs.org/dist/$VERSION/node-$VERSION-$DISTRO.tar.xz
+sudo mkdir -p /usr/local/lib/nodejs
+sudo tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs
+export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
+```
+Install cypress:
+```
+npm install cypress
+```
+Install cypress plugin:
+```
+npm install @4tw/cypress-drag-drop
+```
+
+### Install Chrome
+```
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get -y install google-chrome-stable
+```
+
+### Execute tests
+Cypress expects default localization of tests in folder ```./cypress/integration```.
+The file ./cypress.json defines the defaults - it is expected that FRINX-machine is run locally.
+Then you can run tests interactivelly by issuing the command:
+```
+~/node_modules/.bin/cypress open --browser chrome
+```
+Note: use 'cypress open' for interactive testing
+
+In case you need to test remote FRINX-machine instance
+you can run tests after providing of proper IP and credentials from command line like this:
+```
+CYPRESS_baseUrl=http://10.103.5.231 \
+CYPRESS_inventory=http://10.103.5.231:5601 \
+CYPRESS_login=example@example.com \
+CYPRESS_password=your_password \
+~/node_modules/.bin/cypress run --browser chrome --headless
+```
+Note: use 'cypress run' for automatic testing
+
+[cypress]: https://docs.cypress.io/guides/getting-started/installing-cypress.html
---
+++ cypress/integration/00kibana_fm_init_spec.js
@@ -0,0 +1,52 @@
+describe('setup kibana', function() {
+  it('being FIRST TIME IN KIBANA prepare indices', function() {
+    let inventory = Cypress.env('inventory')
+    cy.visit(inventory)
+    cy.url({timeout:5000}).should('include', '/app/')
+    //this is needed only for the first time
+    cy.contains('Explore on my own',{timeout:10000}).click()
+
+    /*
+    cy.contains('Management',{timeout:10000}).click()
+    cy.contains('Index Patterns',{timeout:10000}).click({force:true})
+    cy.get('button[data-test-subj="createIndexPatternButton"]').click({force:true})
+    cy.get('input[name="indexPattern"][data-test-subj="createIndexPatternNameInput"]').clear({force:true}).type('logstash',{force:true})
+    cy.contains('Success! Your index pattern matches 1 index.')
+    cy.contains('Next step',{timeout:10000}).click({force:true})
+    //cy.contains('Show advanced options',{timeout:10000}).click({force:true})
+    cy.get('select[data-test-subj="createIndexPatternTimeFieldSelect"]').select("I don't want to use the Time Filter",{force:true})
+    cy.get('button[data-test-subj="createIndexPatternCreateButton"]').contains('Create index pattern',{timeout:10000}).click({force:true})
+    */
+  })
+
+  it.skip('deletes index', function() {
+    let inventory = Cypress.env('inventory')
+    cy.visit(inventory)
+    cy.url({timeout:5000}).should('include', '/app/')
+    cy.contains('Management',{timeout:10000}).click()
+    cy.contains('Index Patterns',{timeout:10000}).click({force:true})
+    cy.get('button[data-test-subj="deleteIndexPatternButton"]').click({force:true})
+    cy.get('button[data-test-subj="confirmModalConfirmButton"]').click({force:true})
+  })
+
+  it.skip('deletes index', function() {
+    let inventory = Cypress.env('inventory')
+    cy.visit(inventory)
+    cy.url({timeout:5000}).should('include', '/app/')
+    cy.contains('Management',{timeout:10000}).click()
+    cy.contains('Index Patterns',{timeout:10000}).click({force:true})
+    cy.get('button[data-test-subj="deleteIndexPatternButton"]').click({force:true})
+    cy.get('button[data-test-subj="confirmModalConfirmButton"]').click({force:true})
+  })
+
+  it.skip('deletes index', function() {
+    let inventory = Cypress.env('inventory')
+    cy.visit(inventory)
+    cy.url({timeout:5000}).should('include', '/app/')
+    cy.contains('Management',{timeout:10000}).click()
+    cy.contains('Index Patterns',{timeout:10000}).click({force:true})
+    cy.get('button[data-test-subj="deleteIndexPatternButton"]').click({force:true})
+    cy.get('button[data-test-subj="confirmModalConfirmButton"]').click({force:true})
+  })
+
+})
