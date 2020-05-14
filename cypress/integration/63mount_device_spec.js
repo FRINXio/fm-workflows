4dcced99738fc3ac0b45f59e8ae76abfc86aba38
Stanislav Chlebec <schlebec@frinx.io>
2020-05-14 13:32:11 +0200

    Fix not catching xhr on localhost. Hardening of  tests beginning with 6*.
    - support command cy.login()
      waits for finishing of xhr (it failed without that while testing on localhost)
    - new support function cy.waitForXHR()
      waits for xhr were too quick to catch on localhost
      if localhost server is detected then this function suppress waiting for xhr
    - call cy.waitForXHR() instead of cy.wait(['@getAllStatusCli', '@getAllStatusNetconf'])
      applicable especially everywhere where is present unmounting devices
    - test related to adding devices to repository that were skipped were refactored
      and released to testing process
      they use devices present in frinx infrastructure - so mounting outside will fail
    - the test 40obtain_data_spec.js was failing for viewport 1440*900 beause the attempt
      to scroll unscrollable element was made
    
    Signed-off-by: Stanislav Chlebec <schlebec@frinx.io>

--- cypress/integration/63mount_device_spec.js
+++ cypress/integration/63mount_device_spec.js
@@ -7,21 +7,27 @@ describe('Mount the new device from Inventory', function() {
   })
 	
 
-  it.skip('mount newly added cli device from the inventory', function() {
+  it('mount newly added cli device from the inventory', function() {
+    var device_id='BIG_ONE_ROUTER' //: any unique identifier
+
     cy.server()
     cy.route('POST', '/api/conductor/workflow').as('getWorkflowId')
+    cy.server({
+      method: 'GET',
+    })
+    cy.route('/api/odl/conf/uniconfig/' + device_id).as('getConfigC')
+    cy.route('/api/odl/oper/uniconfig/' + device_id).as('getConfigO')
 
     cy.visit('/')
 
-    cy.contains('Workflows').click()	  
+    cy.contains('Workflows').click()
 
     cy.url().should('include', '/workflows/defs')
     cy.contains('Definitions').click() //there are three tabs: Definitions Executed and Scheduled
-    cy.get('input[placeholder="Search by keyword."').type('Mount_from_inventory')	  
-    cy.contains('Mount_from_inventory').click()	  
-    cy.get('button').contains('Execute').click()	  
+    cy.get('input[placeholder="Search by keyword."').type('Mount_from_inventory')
+    cy.contains('Mount_from_inventory').click()	
+    cy.get('button').contains('Execute').click()
 
-    var device_id='BIG_ONE_ROUTER' //: any unique identifier
     cy.get('label').contains('device_id').next().as('device_id') //label  become alias of next input
     cy.get('@device_id').type('{selectall}{backspace}')
     cy.get('@device_id').type(device_id)
@@ -35,7 +41,7 @@ describe('Mount the new device from Inventory', function() {
     //hopufully now we are ready to go - let us click the workflow id link
     cy.get('div.modal-footer a:first-child').click() //click the ID of the previously executed workflow to see the progress of the workflow
 
-    cy.url().should('include', '/workflows/exec')	  
+    cy.url().should('include', '/workflows/exec')
     cy.get('div.modal-header').contains('Details of Mount_from_inventory',{timeout:30000})
     cy.contains('Execution Flow').click()
     cy.contains('Close').scrollIntoView()
@@ -49,29 +55,39 @@ describe('Mount the new device from Inventory', function() {
     cy.contains('Close').scrollIntoView()
     cy.contains('JSON').click()
     cy.contains('Close').scrollIntoView()
-    //cy.get('div.headerInfo').contains('COMPLETED',{timeout:80000})
+    cy.contains('Execution Flow').click()
+    //cy.get('div[role="dialog"]').scrollTo('bottom', { duration: 1000 })
+    cy.contains('Close').scrollIntoView()
+    cy.get('div.headerInfo').contains('COMPLETED',{timeout:80000})
 
     cy.contains('Close').click()
 
-    cy.get('.navbar-brand').click()	  
-    cy.contains('UniConfig').click()	  
+    cy.get('.navbar-brand').click()
+    cy.contains('UniConfig').click()
+    cy.contains(device_id,{timeout:10000}).parent().find('td').eq(0).click()
+    cy.contains(device_id).parent().find('td').eq(3).contains('connected')
+    cy.contains(device_id).parent().find('td').eq(5).click()
+    cy.wait('@getConfigC')
+    cy.wait('@getConfigO')
+    cy.url().should('include', '/devices/edit/' + device_id)
   })
 
-  it.skip('mount newly added netconf device in the inventory', function() {
+  it('try to mount already MOUNTED newly added cli device from the inventory', function() {
+    var device_id='BIG_ONE_ROUTER' //: any unique identifier
+
     cy.server()
     cy.route('POST', '/api/conductor/workflow').as('getWorkflowId')
 
     cy.visit('/')
 
-    cy.contains('Workflows').click()	  
+    cy.contains('Workflows').click()
 
     cy.url().should('include', '/workflows/defs')
     cy.contains('Definitions').click() //there are three tabs: Definitions Executed and Scheduled
-    cy.get('input[placeholder="Search by keyword."').type('Mount_from_inventory')	  
-    cy.contains('Mount_from_inventory').click()	  
-    cy.get('button').contains('Execute').click()	  
+    cy.get('input[placeholder="Search by keyword."').type('Mount_from_inventory')
+    cy.contains('Mount_from_inventory').click()	
+    cy.get('button').contains('Execute').click()
 
-    var device_id='GREATER_ONE_ROUTER' //: any unique identifier
     cy.get('label').contains('device_id').next().as('device_id') //label  become alias of next input
     cy.get('@device_id').type('{selectall}{backspace}')
     cy.get('@device_id').type(device_id)
@@ -85,7 +101,7 @@ describe('Mount the new device from Inventory', function() {
     //hopufully now we are ready to go - let us click the workflow id link
     cy.get('div.modal-footer a:first-child').click() //click the ID of the previously executed workflow to see the progress of the workflow
 
-    cy.url().should('include', '/workflows/exec')	  
+    cy.url().should('include', '/workflows/exec')
     cy.get('div.modal-header').contains('Details of Mount_from_inventory',{timeout:30000})
     cy.contains('Execution Flow').click()
     cy.contains('Close').scrollIntoView()
@@ -99,11 +115,87 @@ describe('Mount the new device from Inventory', function() {
     cy.contains('Close').scrollIntoView()
     cy.contains('JSON').click()
     cy.contains('Close').scrollIntoView()
-    //cy.get('div.headerInfo').contains('COMPLETED',{timeout:80000})
+    cy.contains('Execution Flow').click()
+    //cy.get('div[role="dialog"]').scrollTo('bottom', { duration: 1000 })
+    cy.contains('Close').scrollIntoView()
+    cy.get('div.headerInfo').contains('FAILED',{timeout:80000})
 
     cy.contains('Close').click()
 
-    cy.get('.navbar-brand').click()	  
-    cy.contains('UniConfig').click()	  
+    cy.get('.navbar-brand').click()
+    cy.contains('UniConfig').click()
+    cy.contains(device_id,{timeout:10000}).parent().find('td').eq(0).click()
+    cy.contains(device_id).parent().find('td').eq(3).contains('connected')
+  })
+
+  it('mount newly added netconf device in the inventory', function() {
+    var device_id='GREATER_ONE_ROUTER' //: any unique identifier
+
+    cy.server()
+    cy.route('POST', '/api/conductor/workflow').as('getWorkflowId')
+    cy.server({
+      method: 'GET',
+    })
+    cy.route('/api/odl/conf/uniconfig/' + device_id).as('getConfigC')
+    cy.route('/api/odl/oper/uniconfig/' + device_id).as('getConfigO')
+
+    cy.visit('/')
+
+    cy.contains('Workflows').click()
+
+    cy.url().should('include', '/workflows/defs')
+    cy.contains('Definitions').click() //there are three tabs: Definitions Executed and Scheduled
+    cy.get('input[placeholder="Search by keyword."').type('Mount_from_inventory')
+    cy.contains('Mount_from_inventory').click()	
+    cy.get('button').contains('Execute').click()
+
+    cy.get('label').contains('device_id').next().as('device_id') //label  become alias of next input
+    cy.get('@device_id').type('{selectall}{backspace}')
+    cy.get('@device_id').type(device_id)
+
+    cy.get('div.modal-content').contains('Execute').click()
+    cy.wait('@getWorkflowId')
+    cy.get('div.modal-content').contains('Execute').should('not.to.exist')
+    cy.get('div.modal-content').contains('OK')
+    //this explicit wait is needed to wait for completing of procesing on chain ConductorServer<->ElasticSearch<->Dyn
+    cy.wait(1000)
+    //hopufully now we are ready to go - let us click the workflow id link
+    cy.get('div.modal-footer a:first-child').click() //click the ID of the previously executed workflow to see the progress of the workflow
+
+    cy.url().should('include', '/workflows/exec')
+    cy.get('div.modal-header').contains('Details of Mount_from_inventory',{timeout:30000})
+    //cy.contains('Execution Flow').click()
+    //cy.contains('Close').scrollIntoView()
+    //cy.contains('Task Details').click()
+    //cy.contains('Close').scrollIntoView()
+    //cy.contains('Execution Flow').click()
+    //cy.contains('Close').scrollIntoView()
+    //cy.contains('Input/Output').click()
+    //cy.contains('Close').scrollIntoView()
+    cy.contains('Execution Flow').click()
+    ////cy.contains('Close').scrollIntoView()
+    //cy.contains('JSON').click()
+    //cy.contains('Close').scrollIntoView()
+    cy.get('div[role="dialog"]').scrollTo('bottom', { duration: 1000 })
+    ////cy.contains('Execution Flow').click()
+    ////cy.contains('Close').scrollIntoView()
+    cy.get('div.headerInfo').contains('COMPLETED',{timeout:180000})
+    cy.contains('Task Details').click()
+    cy.contains('Close').scrollIntoView()
+    cy.contains('Input/Output').click()
+    cy.contains('Close').scrollIntoView()
+    cy.contains('JSON').click()
+    cy.contains('Close').scrollIntoView()
+
+    cy.contains('Close').click()
+
+    cy.get('.navbar-brand').click()
+    cy.contains('UniConfig').click()
+    cy.contains(device_id,{timeout:1000000}).parent().find('td').eq(0).click()
+    cy.contains(device_id).parent().find('td').eq(3).contains('connected')
+    cy.contains(device_id).parent().find('td').eq(5).click()
+    cy.wait('@getConfigC')
+    cy.wait('@getConfigO')
+    cy.url().should('include', '/devices/edit/' + device_id)
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -45,6 +45,7 @@ Cypress.Commands.add("unmount_incompatible_devices", () => {
     cy.visit('/') 
     cy.contains('UniConfig').click()	  
     cy.contains('VRP01').parent().find('td').eq(0).click()
+    cy.contains('Unmount Devices').click()	  
     cy.contains('netconf-testtool').parent().find('td').eq(0).click()
     cy.contains('Unmount Devices').click()	  
     cy.contains('VRP01').should('not.to.exist')
@@ -53,16 +54,49 @@ Cypress.Commands.add("unmount_incompatible_devices", () => {
 
 if (!Cypress.env("SKIP_LOGIN")) {
 Cypress.Commands.add("login", () => {
+    cy.server({
+      method: 'POST',
+    })
+    cy.route('/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBjf7igOYiFFASYpYiNZgWQOPb96Epn1_A').as('getId')
     let login = Cypress.env('login')
     let password= Cypress.env('password')
     cy.visit('/login') 
     cy.get('input[type="email"]').click().type(login)
     cy.get('input[type="password"]').click().type(password, { log: false })
     cy.get('button').contains('Sign In').click()
+    cy.wait('@getId',{timeout:30000})
     cy.contains('Workflows')
 })
 } else {
 Cypress.Commands.add("login", () => {
+;
+})
+}
+
+//this is helper functions to solve very fast xhr on localhost FM server which I am unable to catch
+//and at the same time function to wait for xhr ending on slower connections
+//definition of the function is dependant on the value of CYPRESS_baseUrl=http://10.19.0.5:3000
+//if (!Cypress.env("baseUrl").includes("localhost")) {
+//if (false) {
+if (!Cypress.env("baseUrl")=="http://localhost:3000") {
+Cypress.Commands.add("waitForXHR", () => {
+    cy.server()
+    cy.route('/api/odl/oper/all/status/cli').as('getAllStatusCli')
+    cy.route('/api/odl/oper/all/status//topology-netconf').as('getAllStatusNetconf')
+    //20200514
+    //THIS WORKS ON SLOWER NETWORKS BUT NOT LOCALLY
+    //WHEN RUNNING FM ON localhost THEN THESE XHRs ARE TOO FAST TO CATCH
+    //wait a second for finishing of loading of the list of connected devices
+    //there is two xhr we will wait for and after then 3 times bunch of xhrs
+    cy.wait(['@getAllStatusCli', '@getAllStatusNetconf']).then((xhrs) => {
+      const cliDev = xhrs[0].responseBody.topology[0].node
+      const netconfDev = xhrs[1].responseBody.topology[0].node
+      const rowCount = ((cliDev === undefined) ? 0 : cliDev.length) + ((netconfDev === undefined) ? 0 : netconfDev.length)
+      cy.get('table tbody tr td:first-child', {timeout:5000}).should('have.length', rowCount)
+    })
+})
+} else {
+Cypress.Commands.add("waitForXHR", () => {
 ;
 })
 }
