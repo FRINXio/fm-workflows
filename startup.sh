62408ffa7da039536f8ccd868d9a179a18a13146
vklevlanyk <vklevlanyk@frinx.io>
2020-07-14 08:45:58 +0200

    Add sample-topology module.

--- startup.sh
+++ startup.sh
@@ -22,34 +22,6 @@ docker system prune -f
 # Common functions
 script="startup.sh"
 
-function example {
-    echo -e "example: $script"
-}
-
-function usage {
- echo -e "usage: $script [OPTION]  \n"
-}
-
-function help {
-  usage
-    echo -e "OPTION:"
-    echo -e "  -s | --skip      Skips health check."
-    echo -e "\n"
-  example
-}
-
-function check_success {
-  if [[ $? -ne 0 ]]
-  then
-  RED='\033[0;31m'
-  NC='\033[0m' # No Color
-  printf "${RED}System is not healthy. Shutting down.\n${NC}"
-  echo "Removing containers."
-  ./teardown.sh
-  exit
-fi
-}
-
 function start_bridge_container {
   docker-compose -f docker-compose.bridge.yml up -d "$1"
 }
@@ -61,7 +33,6 @@ function start_host_container {
 function start_containers {
 local containers_to_start=("sample-topology")
 
-
 for i in "${containers_to_start[@]}"; do
 
 if [ "$networking" = "host" ]; then
@@ -72,19 +43,11 @@ else
      echo "Only host is allowed"
      exit  
 fi
-if [ "$skip" = false ]; then
-  ./health_check.sh $i
-  check_success $?
-fi
 echo "################"
 done
-
 }
 
-
 # Loop arguments
-skip=false
-browser=false
 
 while [ "$1" != "" ];
 do
@@ -93,18 +56,6 @@ case $1 in
     networking="$2"
     shift
     shift
-    ;;
-    -s | --skip)
-    skip=true
-    shift
-    ;;
-    -b | --browser)
-    browser=true
-    shift
-    ;;
-    -h | --help )
-    help
-    exit
     ;;
     *)    # unknown option
     echo "$script: illegal option $1"
