94b049a9487241badd109da34ab44bc29a872815
Jozef Rózsa <jrozsa@frinx.io>
2020-12-18 14:17:15 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
+
+ERROR='\033[0;31m[ERROR]:\033[0;0m'
+WARNING='\033[0;33m[WARNING]:\033[0;0m'
+INFO='\033[0;96m[INFO]:\033[0;0m'
+
+echo -e "${INFO} Pulling images"
+docker-compose --log-level ERROR -f composefiles/swarm-fm-workflows.yml pull
