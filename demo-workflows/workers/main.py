dac8036d4b115f2c5cb9bdb880f8fb14cd00511d
Jozef Volak <jvolak@frinx.io>
2021-05-12 14:35:00 +0200

    Add logging classes

--- demo-workflows/workers/main.py
+++ demo-workflows/workers/main.py
@@ -1,41 +1,73 @@
+import os
 import time
+import json
+import logging.config
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
 from conductor.FrinxConductorWrapper import FrinxConductorWrapper
 from workers.frinx_rest import conductor_url_base, conductor_headers
+from workers import import_workflows
+
+from workers import common_worker
+from workers import cli_worker
+from workers import netconf_worker
+from workers import uniconfig_worker
+from workers import http_worker
+
 import inventory_worker
 import lldp_worker
 import platform_worker
-import vll_worker
 import unified_worker
 import psql_worker
+import vll_worker
 import vll_service_worker
 import vpls_worker
 import vpls_service_worker
 import bi_service_worker
 
-from workers import common_worker
-from workers import import_workflows
-from workers import cli_worker
-from workers import netconf_worker
-from workers import uniconfig_worker
-from workers import http_worker
 from importDevices import import_devices
 import netconf_testtool
-import os
 
+# Suppress InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+log = logging.getLogger(__name__)
 
 workflows_folder_path = '../workflows'
 healtchchek_file_path = '../healthcheck'
 
+def setup_logging(
+        default_path='logging-config.json',
+        default_level=logging.INFO,
+        env_key='LOG_CFG'
+):
+    """Setup logging configuration
+
+    """
+    path = os.path.join(os.path.dirname(__file__), default_path)
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
 def main():
+    setup_logging()
+
     if os.path.exists(healtchchek_file_path):
         os.remove(healtchchek_file_path)
 
-
-    print('Starting FRINX workers')
+    log.info('Starting FRINX workers')
     cc = FrinxConductorWrapper(conductor_url_base, 1, 1, headers=conductor_headers)
     cc.start_queue_polling()
     register_workers(cc)
     import_workflows.import_workflows(workflows_folder_path)
+
     import_devices("../devices/cli_device_data.csv", "../devices/cli_device_import.json")
     import_devices("../devices/netconf_device_data.csv", "../devices/netconf_device_import.json")
 
