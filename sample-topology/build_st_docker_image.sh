a83010ada0d932bcb5511440d41e964e0d3bed35
Jozef Volak <jvolak@frinx.io>
2022-01-21 16:38:42 +0100

    Compatible with github actions

--- sample-topology/build_st_docker_image.sh
+++ sample-topology/build_st_docker_image.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-cd ..
-# Get yang schema git submodule
-echo -e "Pulling submodules..."
-if [[ ! -d '.git' ]]; then
-  git clone https://github.com/FRINXio/yang-schemas.git
-else
-  git submodule update --init yang-schemas
-fi
+scriptName="$(basename "${0}")"
+ST_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-echo -e "Copying specific pulled schemas into schema folder..."
-cp -r yang-schemas/cisco-iosxr-653 sample-topology/schemas
-cp -r yang-schemas/cisco-iosxr-663 sample-topology/schemas
-cp -r yang-schemas/junos-16-2021 sample-topology/schemas
+SAMPLE_TOPOLOGY_IMAGE_NAME=${1}
 
-# After all schemas are copied to sample-topology/schemas then build image
-docker build sample-topology/ -t frinx/sample-topology:latest
+pushd ${ST_DIR}/.. 
+
+  # Get yang schema git submodule
+  echo -e "Pulling submodules..."
+  if [[ ! -d '.git' ]]; then
+    git clone https://github.com/FRINXio/yang-schemas.git
+  else
+    git submodule update --init yang-schemas
+  fi
+
+  echo -e "Copying specific pulled schemas into schema folder..."
+  cp -r yang-schemas/cisco-iosxr-653 sample-topology/schemas
+  cp -r yang-schemas/cisco-iosxr-663 sample-topology/schemas
+  cp -r yang-schemas/junos-16-2021 sample-topology/schemas
+
+  # After all schemas are copied to sample-topology/schemas then build image
+  docker build sample-topology/ -t ${SAMPLE_TOPOLOGY_IMAGE_NAME}
+
+popd
