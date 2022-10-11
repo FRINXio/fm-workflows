08ff4434301fb6254d406b2116ca8834f5702188
dbelusky <dbelusky@frinx.io>
2022-10-11 14:38:22 +0200

    divide arangodb delete and create DB backups

--- demo-workflows/workers/topology_discovery_worker.py
+++ demo-workflows/workers/topology_discovery_worker.py
@@ -1,21 +1,40 @@
 import util
 import requests
+import json
 
 TOPOLOGY_DISCOVERY_BASE_URL = "http://fm_topology-discovery:5000/api"
+TOPOLOGY_DISCOVERY_HEADERS = {"Content-Type": "application/json"}
 
 
-def arangodb_create_backup_and_delete_old_backups(task):
-    response = requests.post(TOPOLOGY_DISCOVERY_BASE_URL + "/backup_arangodb")
+def arangodb_create_backup(task):
+    response = requests.post(TOPOLOGY_DISCOVERY_BASE_URL + "/arangodb-backup-create")
+    return util.completed_response(response.json())
+
+
+def arangodb_delete_backups(task):
+    delete_age = task["inputData"]["delete_age"]
+    data = {"delete_age": int(delete_age)}
+    response = requests.delete(TOPOLOGY_DISCOVERY_BASE_URL + "/arangodb-backup-delete", data=json.dumps(data),
+                               headers=TOPOLOGY_DISCOVERY_HEADERS)
+
     return util.completed_response(response.json())
 
 
 def start(cc):
-    cc.register('ArangoDB_create_backup_and_delete_old_backups', {
-        "description": '{"description": "ArangoDB create backup and delete old backups"}',
+    cc.register('Arangodb_create_backup', {
+        "description": '{"description": "ArangoDB create backup"}',
         "inputKeys": [],
         "outputKeys": [
-            "created_db_name",
-            "list_of_deleted_db"
+            "db_name"
         ]
-    }, arangodb_create_backup_and_delete_old_backups)
+    }, arangodb_create_backup)
 
+    cc.register('Arangodb_delete_backups', {
+        "description": '{"description": "ArangoDB delete all backups which are older than param delete_age"}',
+        "inputKeys": [
+            "delete_age"
+        ],
+        "outputKeys": [
+            "deleted_backups"
+        ]
+    }, arangodb_delete_backups)
