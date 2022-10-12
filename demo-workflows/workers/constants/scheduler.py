e1bfca6e7ab09d774dac794a9a442bec257eebd1
dbelusky <dbelusky@frinx.io>
2022-10-12 14:17:53 +0200

    implement scheduler to main.py

---
+++ demo-workflows/workers/constants/scheduler.py
@@ -0,0 +1,14 @@
+UNIFLOW_SCHEDULER_URL = "http://workflow-proxy:8088/schedule"
+
+UNIFLOW_SCHEDULER_LIST = [
+    {
+        "Arangodb_create_backup_and_delete_old_backups": {
+            "workflowName": "Arangodb_create_backup_and_delete_old_backups",
+            "workflowVersion": "1",
+            "workflowContext": {"delete_age": "24"},
+            "name": "Arangodb_create_backup_and_delete_old_backups:1",
+            "cronString": "0 * * * *",
+            "enabled": True,
+        }
+    }
+]
